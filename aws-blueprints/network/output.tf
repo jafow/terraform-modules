940edca93db3cbdfd4c6283c961c9e5d2fa62e57
jafow <jared.a.fowler@gmail.com>
2020-05-19 10:57:58 -0700

    network; output subnet ids and put to ssm

--- aws-blueprints/network/output.tf
+++ aws-blueprints/network/output.tf
@@ -6,6 +6,13 @@ output "private_subnet_cidrs" {
   value = module.subnets.private_subnet_cidrs
 }
 
+output "public_subnet_ids" {
+  value = module.subnets.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.subnets.private_subnet_ids
+}
 output "vpc_cidr" {
   value = module.vpc.vpc_cidr_block
 }
--- aws-blueprints/network/ssm.tf
+++ aws-blueprints/network/ssm.tf
@@ -1,36 +1,55 @@
 
 resource "aws_ssm_parameter" "private_subnet_cidrs" {
-  name = "/${var.stage}/${var.region}/private_subnet_cidrs"
+  name        = "/${var.stage}/${var.region}/private_subnet_cidrs"
   description = "private subnet cidr"
-  type = "SecureString"
-  value = join(",", module.subnets.private_subnet_cidrs)
+  type        = "SecureString"
+  value       = join(",", module.subnets.private_subnet_cidrs)
 
   tags = var.tags
 }
 
 resource "aws_ssm_parameter" "public_subnet_cidrs" {
-  name = "/${var.stage}/${var.region}/public_subnet_cidrs"
+  name        = "/${var.stage}/${var.region}/public_subnet_cidrs"
   description = "public subnet cidr"
-  type = "SecureString"
-  value = join(",", module.subnets.public_subnet_cidrs)
+  type        = "SecureString"
+  value       = join(",", module.subnets.public_subnet_cidrs)
 
   tags = var.tags
 }
 
 resource "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.stage}/${var.region}/vpc_id"
+  name        = "/${var.stage}/${var.region}/vpc_id"
   description = "vpc id"
-  type = "String"
-  value = module.vpc.vpc_id
+  type        = "String"
+  value       = module.vpc.vpc_id
 
   tags = var.tags
 }
 
 resource "aws_ssm_parameter" "igw_id" {
-  name = "/${var.stage}/${var.region}/igw_id"
+  name        = "/${var.stage}/${var.region}/igw_id"
   description = "vpc id"
-  type = "String"
-  value = module.vpc.igw_id
+  type        = "String"
+  value       = module.vpc.igw_id
 
   tags = var.tags
 }
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name        = "/${var.stage}/${var.region}/private_subnet_ids"
+  description = "private subnet cidr"
+  type        = "SecureString"
+  value       = join(",", module.subnets.private_subnet_ids)
+
+  tags = var.tags
+}
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name        = "/${var.stage}/${var.region}/public_subnet_ids"
+  description = "public subnet cidr"
+  type        = "SecureString"
+  value       = join(",", module.subnets.public_subnet_ids)
+  tags        = var.tags
+}
+
+
